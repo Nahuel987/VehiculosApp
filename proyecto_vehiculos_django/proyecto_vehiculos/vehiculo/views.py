78a835693578e58b94cd01b7ff236bb22f37eaf2
Javier Nahuelcheo <sourkuky.gpt@gmail.com>
2024-11-06 03:02:16 -0300

    Se agrega formulario, vista y template

--- proyecto_vehiculos_django/proyecto_vehiculos/vehiculo/views.py
+++ proyecto_vehiculos_django/proyecto_vehiculos/vehiculo/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import VehiculoForm
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def agregar_vehiculo(request):
+    if request.method == 'POST':
+        form = VehiculoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = VehiculoForm()
+    return render(request, 'agregar_vehiculo.html', {'form': form})
